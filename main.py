a5519d7d916f1a9a115f092cfd5b57ccadfc2b87
VictorForYandex <118557158+VictorForYandex@users.noreply.github.com>
2022-11-18 21:10:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ def main():
     print('And I change it every day')
     print('Again')
     print('UFO came and added this line')
+    print('My first git program')
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
 
 
 if __name__ == '__main__':
